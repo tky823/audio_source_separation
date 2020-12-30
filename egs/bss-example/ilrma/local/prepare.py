82e4346a12930ffbaa039e2e6b3768aa80d20ccf
tky823 <40362510+tky823@users.noreply.github.com>
2020-12-30 14:20:40 +0900

    fix typo

--- egs/bss-example/ilrma/local/prepare.py
+++ egs/bss-example/ilrma/local/prepare.py
@@ -6,11 +6,11 @@ import librosa
 def main():
     target_sr = 16000
 
-    titles = ['man-16000', 'woman-16000']
+    titles = ['man', 'woman']
 
     for title in titles:
-        source, sr = librosa.load("./data/{}.mp3".format(title), target_sr)
-        librosa.write_wav("./data/{}.wav".format(title), source, target_sr)
+        source, sr = librosa.load("./data/{}-44100.mp3".format(title), target_sr)
+        librosa.write_wav("./data/{}-16000.wav".format(title), source, target_sr)
     
 if __name__ == '__main__':
     main()
