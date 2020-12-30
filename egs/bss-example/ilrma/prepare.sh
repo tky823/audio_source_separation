d2c3e317c379d919eb5eaa7690b3cf71f8b065ae
tky823 <40362510+tky823@users.noreply.github.com>
2020-12-30 14:18:19 +0900

    Add examples

--- egs/bss-example/ilrma/prepare.sh
+++ egs/bss-example/ilrma/prepare.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PYTHONPATH="./local:$PYTHONPATH"
+
 mkdir "data"
 wget "https://soundeffect-lab.info/sound/voice/mp3//game/wizard-greeting1.mp3" -O "data/man-44100.mp3"
 wget "https://soundeffect-lab.info/sound/voice/mp3//game/swordwoman-win1.mp3" -O "data/woman-44100.mp3"
