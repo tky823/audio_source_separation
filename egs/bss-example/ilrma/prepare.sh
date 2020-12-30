dfae9f61a1581a9cf43f376cb53b061c0a604be6
tky823 <40362510+tky823@users.noreply.github.com>
2020-12-30 14:05:33 +0900

    Add script

---
+++ egs/bss-example/ilrma/prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir "data"
+wget "https://soundeffect-lab.info/sound/voice/mp3//game/wizard-greeting1.mp3" -O "data/man-44100.mp3"
+wget "https://soundeffect-lab.info/sound/voice/mp3//game/swordwoman-win1.mp3" -O "data/woman-44100.mp3"
