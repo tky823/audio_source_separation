eba25589caf946c10d5eea851bbeddbbe49cd60d
tky823 <40362510+tky823@users.noreply.github.com>
2021-01-02 10:16:12 +0900

    add args

--- egs/bss-example/fdica/local/prepare.py
+++ egs/bss-example/fdica/local/prepare.py
@@ -1,46 +1,60 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
 import os
 import numpy as np
 from scipy.io import loadmat
 import librosa
 
-def main():
-    titles = ['aew', 'axb', 'bdl']
+parser = argparse.ArgumentParser(description="Example of frequency-domain ICA (FDICA)")
+
+parser.add_argument('--data_root', type=str, default=None, help='Path for dataset ROOT directory.')
+parser.add_argument('--titles', type=str, default="[aew,axb,bdl]", help='Path for dataset ROOT directory.')
+parser.add_argument('--reverb', type=float, default=0.16, help='The reverberation time (T60).')
+parser.add_argument('--duration', type=float, default=0.5, help='The truncating time of impulse response.')
+parser.add_argument('--mic_intervals', type=str, default="8-8-8-8-8-8-8", help='The microphone intervals.')
+
+def main(args):
+    data_root = args.data_root
+    titles = args.titles.replace('[','').replace(']','').split(',')
     target_sr = 16000
     T_min = None
 
     # Resample
     for idx, title in enumerate(titles):
-        source, sr = librosa.load("./data/cmu_us_{}_arctic/wav/arctic_a{:04d}.wav".format(title, idx+1), target_sr)
+        path = os.path.join(data_root, "cmu_us_{}_arctic/wav/arctic_a{:04d}.wav".format(title, idx+1))
+        source, sr = librosa.load(path, target_sr)
         T = len(source)
 
         if T_min is None or T < T_min:
             T_min = T
     
     for idx, title in enumerate(titles):
-        os.makedirs("./data/cmu_us_{}_arctic/trimmed".format(title), exist_ok=True)
-        
-        source, sr = librosa.load("./data/cmu_us_{}_arctic/wav/arctic_a{:04d}.wav".format(title, idx+1), target_sr)
-        librosa.output.write_wav("./data/cmu_us_{}_arctic/trimmed/source-{}.wav".format(title, target_sr), source[:T_min], target_sr)
+        path = os.path.join(data_root, "cmu_us_{}_arctic/wav/arctic_a{:04d}.wav".format(title, idx+1))
+        source, sr = librosa.load(path, target_sr)
+
+        path = os.path.join(data_root, "cmu_us_{}_arctic/trimmed".format(title, idx+1))
+        os.makedirs(path, exist_ok=True)
+        path = os.path.join(data_root, "cmu_us_{}_arctic/trimmed/source-{}.wav".format(title, target_sr))
+        librosa.output.write_wav(path, source[:T_min], target_sr)
 
     # Room impulse response
-    reverb = 0.16
-    duration = 0.5
+    reverb = args.reverb
+    duration = args.duration
     samples = int(duration * target_sr)
-    mic_intervals = "3-3-3-8-3-3-3"
+    mic_intervals = args.mic_intervals
     mic_indices = list(range(8))
     degrees = [0, 15, 30, 45, 60, 75, 90, 270, 285, 300, 315, 330]
     
     for title in titles:
         for degree in degrees:
             for mic_idx in mic_indices:
-                convolve_mird(title, reverb=reverb, degree=degree, mic_intervals=mic_intervals, mic_idx=mic_idx, sr=target_sr, samples=samples)
+                convolve_mird(data_root, title, reverb=reverb, degree=degree, mic_intervals=mic_intervals, mic_idx=mic_idx, sr=target_sr, samples=samples)
 
 
-def convolve_mird(title, reverb=0.160, degree=0, mic_intervals="3-3-3-8-3-3-3", mic_idx=0, sr=16000, samples=None):
-    rir_path = "data/MIRD/Reverb{:.3f}_{}/Impulse_response_Acoustic_Lab_Bar-Ilan_University_(Reverberation_{:.3f}s)_{}_1m_{:03d}.mat".format(reverb, mic_intervals, reverb, mic_intervals, degree)
+def convolve_mird(data_root, title, reverb=0.160, degree=0, mic_intervals="3-3-3-8-3-3-3", mic_idx=0, sr=16000, samples=None):
+    rir_path = os.path.join(data_root, "MIRD/Reverb{:.3f}_{}/Impulse_response_Acoustic_Lab_Bar-Ilan_University_(Reverberation_{:.3f}s)_{}_1m_{:03d}.mat".format(reverb, mic_intervals, reverb, mic_intervals, degree))
     rir_mat = loadmat(rir_path)
 
     rir = rir_mat['impulse_response']
@@ -48,10 +62,14 @@ def convolve_mird(title, reverb=0.160, degree=0, mic_intervals="3-3-3-8-3-3-3", 
     if samples is not None:
         rir = rir[:samples]
 
-    source, sr = librosa.load("data/cmu_us_{}_arctic/trimmed/source-{}.wav".format(title, sr), sr)
+    wav_path = os.path.join(data_root, "cmu_us_{}_arctic/trimmed/source-{}.wav".format(title, sr))
+    source, sr = librosa.load(wav_path, sr)
     convolved_signals = np.convolve(source, rir[:, mic_idx])
 
-    librosa.output.write_wav("./data/cmu_us_{}_arctic/trimmed/convolved-{}_deg{}-mic{}.wav".format(title, sr, degree, mic_idx), convolved_signals, sr)
+    wav_path = os.path.join(data_root, "cmu_us_{}_arctic/trimmed/convolved-{}_deg{}-mic{}.wav".format(title, sr, degree, mic_idx))
+    librosa.output.write_wav(wav_path, convolved_signals, sr)
     
 if __name__ == '__main__':
+    args = parser.parse_args()
+    print(args)
     main()
--- egs/bss-example/fdica/prepare.sh
+++ egs/bss-example/fdica/prepare.sh
@@ -2,23 +2,31 @@
 
 export PATH="./local:$PATH"
 
+data_root="data"
+titles="[aew,axb,bdl]"
 reverb=0.160
+duration=0.5
 mic_intervals='3-3-3-8-3-3-3'
 
-mkdir -p "data"
+mkdir -p ${data_root}
 
-wget "http://festvox.org/cmu_arctic/packed/cmu_us_aew_arctic.tar.bz2" -O "data/cmu_us_aew_arctic.tar.bz2"
-wget "http://festvox.org/cmu_arctic/packed/cmu_us_axb_arctic.tar.bz2" -O "data/cmu_us_axb_arctic.tar.bz2"
-wget "http://festvox.org/cmu_arctic/packed/cmu_us_bdl_arctic.tar.bz2" -O "data/cmu_us_bdl_arctic.tar.bz2"
+wget "http://festvox.org/cmu_arctic/packed/cmu_us_aew_arctic.tar.bz2" -O "${data_root}/cmu_us_aew_arctic.tar.bz2"
+wget "http://festvox.org/cmu_arctic/packed/cmu_us_axb_arctic.tar.bz2" -O "${data_root}/cmu_us_axb_arctic.tar.bz2"
+wget "http://festvox.org/cmu_arctic/packed/cmu_us_bdl_arctic.tar.bz2" -O "${data_root}/cmu_us_bdl_arctic.tar.bz2"
 
-tar -xvf "data/cmu_us_aew_arctic.tar.bz2" -C "data"
-tar -xvf "data/cmu_us_axb_arctic.tar.bz2" -C "data"
-tar -xvf "data/cmu_us_bdl_arctic.tar.bz2" -C "data"
+tar -xvf "${data_root}/cmu_us_aew_arctic.tar.bz2" -C "${data_root}"
+tar -xvf "${data_root}/cmu_us_axb_arctic.tar.bz2" -C "${data_root}"
+tar -xvf "${data_root}/cmu_us_bdl_arctic.tar.bz2" -C "${data_root}"
 
-mkdir -p "data/MIRD/Reverb${reverb}_${mic_intervals}"
+mkdir -p "${data_root}/MIRD/Reverb${reverb}_${mic_intervals}"
 url="https://www.iks.rwth-aachen.de/fileadmin/user_upload/downloads/forschung/tools-downloads/Impulse_response_Acoustic_Lab_Bar-Ilan_University__Reverberation_${reverb}s__${mic_intervals}.zip"
-mkdir -p "data/MIRD/Reverb${reverb}_${mic_intervals}"
-wget ${url} -O "data/MIRD/mird.zip"
-unzip "data/MIRD/mird.zip" -d "data/MIRD/Reverb${reverb}_${mic_intervals}" 
+mkdir -p "${data_root}/MIRD/Reverb${reverb}_${mic_intervals}"
+wget ${url} -O "${data_root}/MIRD/mird.zip"
+unzip "${data_root}/MIRD/mird.zip" -d "${data_root}/MIRD/Reverb${reverb}_${mic_intervals}" 
 
-prepare.py
+prepare.py \
+--data_root ${data_root} \
+--titles ${titles} \
+--reverb ${reverb} \
+--duration ${duration} \
+--mic_intervals ${mic_intervals}
