6c5221c9a68982f8b2d7da0aad8688b3da5ff641
tky823 <40362510+tky823@users.noreply.github.com>
2021-01-03 14:10:15 +0900

    bug fixes

--- src/algorithm/projection_back.py
+++ src/algorithm/projection_back.py
@@ -35,22 +35,6 @@ def projection_back(Y, reference):
         raise ValueError("reference.ndim is expected 2 or 3, but given {}.".format(n_dims))
 
     return scale
-"""
-# pyroomacoustics
-def projection_back(Y, reference):
-    Args:
-        Y: (n_channels, n_bins, n_frames)
-        reference: (n_bins, n_frames)
-    Returns:
-        scale: (n_channels, n_bins)
-    numerator = np.sum(Y * reference.conj(), axis=2) # (n_channels, n_bins)
-    denominator = np.sum(np.abs(Y)**2, axis=2) # (n_channels, n_bins)
-    scale = np.ones((n_channels, n_bins), dtype=np.complex128)
-    indices = denominator > 0.0
-    scale[indices] = numerator[indices] / denominator[indices]
-
-    return scale
-"""
 
 if __name__ == '__main__':
     a = np.array([[1,2], [3,4]])
