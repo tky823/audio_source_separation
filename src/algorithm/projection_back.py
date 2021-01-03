1cb1618f2d17ae6e48a0a4ff361ded3d03d4635a
tky823 <40362510+tky823@users.noreply.github.com>
2021-01-03 16:45:55 +0900

    bug fixes

--- src/algorithm/projection_back.py
+++ src/algorithm/projection_back.py
@@ -14,7 +14,7 @@ def projection_back(Y, reference):
     if n_dims == 2:
         # Y: (n_channels, n_bins, n_frames)
         # X: (1, n_bins, n_frames)
-        X = reference[np.newaxis,:,:].transpose(1,0,2) # (n_sources, n_bins, n_frames)
+        X = reference[np.newaxis,:,:].transpose(1,0,2) # (n_bins, n_channels, n_frames)
         Y = Y.transpose(1,0,2) # (n_bins, n_sources, n_frames)
         Y_Hermite = Y.transpose(0,2,1).conj() # (n_bins, n_frames, n_sources)
         YY_Hermite_inverse = np.linalg.inv(Y @ Y_Hermite) # (n_bins, n_sources, n_sources)
@@ -23,7 +23,7 @@ def projection_back(Y, reference):
     elif n_dims == 3:
         # Y: (n_channels, n_bins, n_frames)
         # X: (n_sources, n_bins, n_frames)
-        X = reference.transpose(1,0,2) # (n_sources, n_bins, n_frames)
+        X = reference.transpose(1,0,2) # (n_bins, n_channels, n_frames)
         Y = Y.transpose(1,0,2) # (n_bins, n_sources, n_frames)
         Y_Hermite = Y.transpose(0,2,1).conj() # (n_bins, n_frames, n_sources)
         YY_Hermite_inverse = np.linalg.inv(Y @ Y_Hermite) # (n_bins, n_sources, n_sources)
