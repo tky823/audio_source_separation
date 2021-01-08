8976fcf59d1158788684304b47a1d20991010111
tky823 <40362510+tky823@users.noreply.github.com>
2021-01-09 02:17:37 +0900

    add distance option

--- egs/bss-example/fdica/local/prepare.py
+++ egs/bss-example/fdica/local/prepare.py
@@ -14,6 +14,7 @@ parser.add_argument('--titles', type=str, default="aew axb bdl", help='Path for 
 parser.add_argument('--reverb', type=float, default=0.16, help='The reverberation time (T60).')
 parser.add_argument('--duration', type=float, default=0.5, help='The trimming time of impulse response.')
 parser.add_argument('--mic_intervals', type=str, default="8-8-8-8-8-8-8", help='The microphone intervals.')
+parser.add_argument('--distance', type=float, default=1, help='The distance between micprophone and sources.')
 
 def main(args):
     data_root = args.data_root
@@ -45,16 +46,17 @@ def main(args):
     samples = int(duration * target_sr)
     mic_intervals = args.mic_intervals
     mic_indices = list(range(8))
+    distance = args.distance
     degrees = [0, 15, 30, 45, 60, 75, 90, 270, 285, 300, 315, 330, 345]
     
     for title in titles:
         for degree in degrees:
             for mic_idx in mic_indices:
-                convolve_mird(data_root, title, reverb=reverb, degree=degree, mic_intervals=mic_intervals, mic_idx=mic_idx, sr=target_sr, samples=samples)
+                convolve_mird(data_root, title, reverb=reverb, degree=degree, mic_intervals=mic_intervals, mic_idx=mic_idx, distance=distance, sr=target_sr, samples=samples)
 
 
-def convolve_mird(data_root, title, reverb=0.160, degree=0, mic_intervals="3-3-3-8-3-3-3", mic_idx=0, sr=16000, samples=None):
-    rir_path = os.path.join(data_root, "MIRD/Reverb{:.3f}_{}/Impulse_response_Acoustic_Lab_Bar-Ilan_University_(Reverberation_{:.3f}s)_{}_1m_{:03d}.mat".format(reverb, mic_intervals, reverb, mic_intervals, degree))
+def convolve_mird(data_root, title, reverb=0.160, degree=0, mic_intervals="3-3-3-8-3-3-3", mic_idx=0, distance=1, sr=16000, samples=None):
+    rir_path = os.path.join(data_root, "MIRD/Reverb{:.3f}_{}/Impulse_response_Acoustic_Lab_Bar-Ilan_University_(Reverberation_{:.3f}s)_{}_{:.0f}m_{:03d}.mat".format(reverb, mic_intervals, reverb, mic_intervals, distance, degree))
     rir_mat = loadmat(rir_path)
 
     rir = rir_mat['impulse_response']
