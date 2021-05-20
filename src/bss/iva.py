0bfde681946ba10a2a86b913c2ff9f9a32530cd9
tky823 <40362510+tky823@users.noreply.github.com>
2021-05-20 20:38:10 +0900

    add AuxGaussIVA

--- src/bss/iva.py
+++ src/bss/iva.py
@@ -329,11 +329,60 @@ class AuxLaplaceIVA(AuxIVAbase):
 class AuxGaussIVA(AuxIVAbase):
     def __init__(self, algorithm_spatial='IP', reference_id=0, callback=None, eps=EPS, threshold=THRESHOLD):
         super().__init__(algorithm_spatial=algorithm_spatial, reference_id=reference_id, callback=callback, eps=eps, threshold=threshold)
-
-        raise NotImplementedError("in progress")
     
     def update_once(self):
-        raise NotImplementedError("in progress...")
+        n_sources, n_channels = self.n_sources, self.n_channels
+        n_bins = self.n_bins
+        eps, threshold = self.eps, self.threshold
+
+        X, W = self.input, self.demix_filter
+        Y = self.estimation
+
+        if not self.algorithm_spatial in __algorithm_spatial__:
+            raise ValueError("Not support {} based spatial updates.".format(self.algorithm_spatial))
+        assert self.algorithm_spatial == 'IP', "Only `IP` is supported."
+        
+        X = X.transpose(1,2,0) # (n_bins, n_frames, n_channels)
+        X = X[...,np.newaxis]
+        X_Hermite = X.transpose(0,1,3,2).conj()
+        XX = X @ X_Hermite # (n_bins, n_frames, n_channels, n_channels)
+        P = np.abs(Y)**2 # (n_sources, n_bins, n_frames)
+        R = P.mean(axis=1)[:,np.newaxis,:,np.newaxis,np.newaxis] # (n_sources, 1, n_frames, 1, 1)
+        R[R < eps] = eps
+        U = XX / R # (n_sources, n_bins, n_frames, n_channels, n_channels)
+        U = U.mean(axis=2) # (n_sources, n_bins, n_channels, n_channels)
+        E = np.eye(n_sources, n_channels)
+        E = np.tile(E, reps=(n_bins,1,1)) # (n_bins, n_sources, n_channels)
+
+        for source_idx in range(n_sources):
+            # W: (n_bins, n_sources, n_channels), U: (n_sources, n_bins, n_channels, n_channels)
+            w_n_Hermite = W[:,source_idx,:] # (n_bins, n_channels)
+            U_n = U[source_idx] # (n_bins, n_channels, n_channels)
+            WU = W @ U_n # (n_bins, n_sources, n_channels)
+            condition = np.linalg.cond(WU) < threshold # (n_bins,)
+            condition = condition[:,np.newaxis] # (n_bins, 1)
+            e_n = E[:,source_idx,:]
+            w_n = np.linalg.solve(WU, e_n)
+            wUw = w_n[:,np.newaxis,:].conj() @ U_n @ w_n[:,:,np.newaxis]
+            denominator = np.sqrt(wUw[...,0])
+            w_n_Hermite = np.where(condition, w_n.conj() / denominator, w_n_Hermite)
+            # if condition number is too big, `denominator[denominator < eps] = eps` may occur divergence of cost function.
+            W[:,source_idx,:] = w_n_Hermite
+        
+        self.demix_filter = W
+
+        X = self.input
+        Y = self.separate(X, demix_filter=W)
+        
+        self.estimation = Y
+    
+    def compute_negative_loglikelihood(self):
+        X, W = self.input, self.demix_filter
+        Y = self.separate(X, demix_filter=W)
+        P = np.mean(np.abs(Y)**2, axis=1)
+        loss = 2 * np.sum(P, axis=0).mean() - 2 * np.log(np.abs(np.linalg.det(W))).sum()
+
+        return loss
 
 class SparseAuxIVA(AuxIVAbase):
     """
